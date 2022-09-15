b4200b14c3d3b597846e0351ac9599db4428ee60
Lenneth <lenneth.deryck@student.hogent.be>
2022-09-15 12:08:31 +0200

    Zoek mogelijkheid
    Mogelijkheid om recepten te zoeken via een zoekbalk

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,40 @@ import { Layout } from "../components/Layout";
 import { Banner } from "../components/Banner";
 import { components } from "../slices";
 import Recipe from '../components/Recipe'
-import {useState} from "react";
+import {useState, useCallback, useEffect} from "react";
 
 
 //The homepage component
 const Index = ({recipes, navigation, settings }) => {
   const [recipesLength, setRecipesLength] = useState(3)
   const [showButton, setShowButton] = useState(true);
+  const [recipeList, setRecipeList] = useState(recipes);
+
+  const [text, setText] = useState("");
+  const [search, setSearch] = useState("");
+
+  const handleInputChange = useCallback((e) => setText(e.target.value), []);
+  const handleSearch = useCallback(() => {setSearch(text)}, [text]);
+
+  // const testList = recipes.filter((recipe) => {
+  //   console.log(recipe.tags);
+  //   console.log(search);
+  //   return recipe.tags.includes(search);
+  // })
+  // console.log(testList)
+
+  useEffect(() => {
+    const filteredRecipes = []
+    if(search){
+      filteredRecipes = recipes.filter((recipe) => recipe.tags.find((t) => t.includes(search.toLocaleLowerCase())) !== undefined)
+    } else {
+      filteredRecipes = recipes
+    }
+    setRecipeList(filteredRecipes);
+  },[search]);
+  useEffect(() => {
+    setRecipeList(recipes);
+  },[]);
 
   //Loads more recipes when the "Load more" button is clicked
   const showMoreRecipes = () =>{
@@ -39,10 +66,29 @@ const Index = ({recipes, navigation, settings }) => {
       <div className="container">
         <Banner settings={settings}/>
         <SliceZone slices={settings?.data.slices} components={components} />
+        <div className="home-recipes-header">
+          <h2>Recipes</h2>
+          <div className="home-recipes-search">
+            <input
+              type="search"
+              value={text}
+              onChange={handleInputChange}
+              className="search-input"
+              placeholder="Search for a transaction"
+            />
+            <button
+              type="button"
+              onClick={handleSearch}
+              className="search-btn"
+            >
+              Search
+            </button>
+          </div>
+        </div>
         <ul className="recipe">
-          {recipes.slice(0, recipesLength).map((recipe) => (
+          {recipeList?recipeList.slice(0, recipesLength).map((recipe) => (
             <Recipe key={recipe?.id} recipe={recipe} />
-          ))}
+          )):"No recipes found"}
         </ul>
         <div className="btn-container"><button className={`btn btn-primary ${showButton?"":"disable"}`} onClick={() => showMoreRecipes()}>Load more</button></div>
         
